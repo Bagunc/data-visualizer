cb8ad00d02faa2139cf601778207447c981b5e60
= <=>
2023-07-20 03:19:49 +0400

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/data-visualizer/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
